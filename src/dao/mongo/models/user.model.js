9d696bd93ff299e6645b181a87b7506a4b74a66e
Anabela Gemignani <anabelag1991@gmail.com>
2023-07-27 22:11:18 -0300

    Initial Commit

---
+++ src/dao/mongo/models/user.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userCollection = 'users';
+
+const userSchema = new mongoose.Schema({
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    email:  {type: String, required: true},
+    password: {type: String, required: true},
+    rol: { type: String, default: 'usuario' },
+});
+  
+export const userModel = mongoose.model(userCollection, userSchema);
